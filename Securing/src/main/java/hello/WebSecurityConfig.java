533bddb4b0b07a1a4a242e8cb3f83f6949f31c4a
liujiajia <liujiajia@mbinfo.cn>
2017-05-31 14:23:08 +0800

    fromat WebSecurityConfig

--- Securing/src/main/java/hello/WebSecurityConfig.java
+++ Securing/src/main/java/hello/WebSecurityConfig.java
@@ -13,15 +13,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
+            .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+            .formLogin()
                 .loginPage("/login")
                 .permitAll()
                 .and()
-                .logout()
+            .logout()
                 .permitAll();
     }
 
